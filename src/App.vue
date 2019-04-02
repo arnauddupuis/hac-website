c1bfa62676c285a047dc8c90d624818eec6b7194
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-04-01 18:08:08 -0700

    :zap: Add basic structure to the site

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,70 @@
 <template>
-  <v-app>
+  <v-app dark>
+    <v-navigation-drawer v-model="sidebar" app>
+      <v-list>
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+      <span class="hidden-sm-and-up">
+        <v-toolbar-side-icon @click="sidebar = !sidebar">
+        </v-toolbar-side-icon>
+      </span>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          {{ appTitle }}
+        </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          flat
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          <v-icon left dark>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-
+    
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
+    
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   },
-  data () {
-    return {
-      //
-    }
+  methods: {
+  },
+  data: () => ({
+    sidebar: false,
+    appTitle: "Hyrule Astronomy Club",
+    menuItems: [
+      { title: 'Home', path: '/', icon: 'home' },
+      { title: 'Documentation', path: '/doc', icon: 'library_books' },
+      { title: 'Ressources', path: '/ressources', icon: 'attachment' }
+    ]
+  }),
+  props: {
+    source: String
   }
 }
 </script>

904becd2219bccb21ace9945e0c231ffe5fb1b17
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-04-26 19:00:04 -0700

    :sparkles: Modified the Youtube component so it is now fluid

--- docs/.vuepress/components/Youtube.vue
+++ docs/.vuepress/components/Youtube.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <iframe width="560" height="315" :src="url" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div class="videoWrapper">
+        <iframe class="fluid-iframe" width="560" height="349" :src="url" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </template>
 

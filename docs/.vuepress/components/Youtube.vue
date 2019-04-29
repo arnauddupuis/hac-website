ed09ec31229306b2afd345c28697fdf7b6edc9be
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-04-29 07:39:17 -0700

    :pencil: give credit for Youtube fluid css

--- docs/.vuepress/components/Youtube.vue
+++ docs/.vuepress/components/Youtube.vue
@@ -1,3 +1,4 @@
+// This trick was originally published on css-tricks: https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
 <template>
     <div class="videoWrapper">
         <iframe width="560" height="349" :src="url" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>

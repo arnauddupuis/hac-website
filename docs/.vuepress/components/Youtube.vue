3da024ad67ae16e81f5fa587b8603ac548e014b9
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-04-29 07:02:20 -0700

    :zap: Youtube component is now completely independant.

--- docs/.vuepress/components/Youtube.vue
+++ docs/.vuepress/components/Youtube.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="videoWrapper">
-        <iframe class="fluid-iframe" width="560" height="349" :src="url" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="349" :src="url" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </template>
 
@@ -9,3 +9,21 @@ export default {
     props: ['url']
 }
 </script>
+
+<style>
+.videoWrapper {
+	position: relative;
+	padding-bottom: 56.25%;
+	padding-top: 25px;
+	height: 0;
+}
+.videoWrapper iframe {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+}
+
+
+</style>

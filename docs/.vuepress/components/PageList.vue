a5b1884f60a7aa6b84088c8e5529a69f2e57ea77
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-05-08 11:13:04 -0700

    :zap: remove useless console log

--- docs/.vuepress/components/PageList.vue
+++ docs/.vuepress/components/PageList.vue
@@ -34,14 +34,13 @@ export default {
 			let files = this.$site.pages.filter(p => {
 				return p.path.indexOf(this.folder) >= 0 && p.frontmatter.published == true;
 			}).sort((a,b) => {
-                let aDate = new Date(a.frontmatter.lastUpdated).getTime();
-                let bDate = new Date(b.frontmatter.lastUpdated).getTime();
-                let diff = aDate - bDate;
+				let aDate = new Date(a.frontmatter.lastUpdated).getTime();
+				let bDate = new Date(b.frontmatter.lastUpdated).getTime();
+				let diff = aDate - bDate;
 				if(diff > 0) return -1;
 				if(diff < 0) return 1;
 				return 0;
-			}).slice(0,5);
-			console.log(files.length)
+			});
 			return files;
 		}
 	}
